1fd43094bd39266c036bed1152da4001dc304cb8
Yosef Yudha Wijaya <yosef@yudhawijaya.com>
2021-11-20 13:04:18 +0700

    initial game engine

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,8 +1,13 @@
 <script setup>
 import { computed, ref } from 'vue'
 import chunk from 'lodash.chunk'
+import intersectionWith from 'lodash.intersectionwith'
 
+// board setups
 const columns = ref(3)
+const player = ref(1)
+const playerOneCells = ref([])
+const playerTwoCells = ref([])
 
 const cellNumbers = computed(() => {
   return Math.pow(columns.value, 2)
@@ -42,11 +47,80 @@ const winningCombinations = computed(() => {
   ]
 })
 
+const winnerLabel = computed(() => {
+  let res
+  const items = winningCombinations.value
+  const playerOne = items.find(item => {
+    const intersection = intersectionWith(item, playerOneCells.value)
+    if (intersection.length === columns.value) { return item }
+  })
+
+  const playerTwo = items.find(item => {
+    const intersection = intersectionWith(item, playerTwoCells.value)
+    if (intersection.length === columns.value) { return item }
+  })
+
+  if (playerOne) {
+    res = 'Player 1 wins'
+  }
+
+  if (playerTwo) {
+    res = 'Player 2 wins'
+  }
+
+  return res
+})
+
+// methods
+const label = (key) => {
+  let label = ''
+  if (playerOneCells.value.includes(key)) {
+    label = 'X'
+  }
+
+  if (playerTwoCells.value.includes(key)) {
+    label = 'O'
+  }
+
+  return label
+}
+const select = (key) => {
+  if (player.value === 1) {
+    playerOneCells.value.push(key)
+    playerOneCells.value.sort()
+  } else {
+    playerTwoCells.value.push(key)
+    playerTwoCells.value.sort()
+  }
+
+  player.value = player.value === 1 ? 2 : 1
+}
+
 </script>
 
 <template>
-  <div class="flex items-center justify-center w-full max-w-md">
-    {{ winningCombinations }}
+  <div class="flex flex-col items-center h-full justify-center w-full">
+    <h2>Player {{ player }} turn</h2>
+    <div class="grid grid-cols-3 gap-0 mx-auto">
+      <div
+        v-for="i in cellNumbers"
+        :key="`cell-${i}`"
+        class="border border-black cursor-pointer flex items-center justify-center h-16 w-16"
+        @click.once="select(i)"
+      >
+        {{ label(i) }}
+      </div>
+    </div>
+    <div>
+      playerOneCells: {{ playerOneCells }}
+    </div>
+    <div>
+      playerTwoCells: {{ playerTwoCells }}
+    </div>
+    <div>{{ winningCombinations }}</div>
+    <div>
+      {{ winnerLabel }}
+    </div>
   </div>
 </template>
 
