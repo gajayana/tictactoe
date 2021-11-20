628d6bf37e3e05c89e98527e993e7ec83f77e28f
Yosef Yudha Wijaya <yosef@yudhawijaya.com>
2021-11-20 21:30:48 +0700

    Add landing page

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import { computed } from 'vue'
+import { useStore } from 'vuex'
 import Board from './components/Board.vue'
+import Landing from './components/Landing.vue'
+
+const store = useStore()
+const gameIsStarted = computed(() => store.state.game.gameIsStarted)
 </script>
 
 <template>
-  <Board />
+  <Board v-if="gameIsStarted" />
+  <Landing v-else />
 </template>
---
+++ src/components/Landing.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { ref, watchEffect } from 'vue'
+import { useStore } from 'vuex'
+
+const store = useStore()
+const size = ref(5)
+const sizes = ref([3,4,5,6])
+
+watchEffect(() => store.commit('game/setColumn', size.value))
+
+const start = () => {
+  store.commit('game/setGameIsStarted', true)
+}
+</script>
+<template>
+  <div class="flex items-center h-full justify-center">
+    <div class="flex flex-col items-center">
+      <h1 class="font-bold mb-8 text-4xl">Tic Tac Toe</h1>
+      <p class="mb-4">Select your columns and start the game!</p>
+      <select
+        v-model.number="size"
+        class="border border-gray-300 mb-4 px-8 py-2 text-4xl"
+      >
+        <option
+          v-for="size in sizes"
+          :key="`size-${size}`"
+        >
+          {{ size }}
+        </option>
+      </select>
+      <button
+        class="bg-green-900 text-white uppercase px-16 py-4 rounded"
+        @click.prevent="start"
+      >
+        Start Game
+      </button>
+    </div>
+  </div>
+</template>
